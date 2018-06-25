5f84cd3525c72f2cec68704b362c286da1e0cf3f
Evgeniy Mokrushin <e.v.mokrushin@chelindbank.ru>
2018-06-25 19:36:20 +0500

    Added field for setting meta keywords.

--- app/Epiphany.SeoMetadata/SeoMetadata.cs
+++ app/Epiphany.SeoMetadata/SeoMetadata.cs
@@ -4,6 +4,7 @@
     {
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Keywords { get; set; }
         public bool NoIndex { get; set; }
         public string UrlName { get; set; }
     }
